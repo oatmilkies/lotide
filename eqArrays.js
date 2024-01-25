2fab6e45d669eadfdfa672eb8c273655076689cd
Leilani <pquaby@gmail.com>
2024-01-25 17:05:35 -0500

    cleaned up code

--- eqArrays.js
+++ eqArrays.js
@@ -15,12 +15,12 @@ const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   } else //Compare each element of the arrays
-    for (i = 0; i < arr1.length; i++) {
+    for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] !== arr2[i]) {
         return false;
       }
     }
-    return true;
+  return true;
 };
 
 //Test cases
