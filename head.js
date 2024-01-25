bc185236e655eef59cb31714781c68713d927cb4
Leilani <pquaby@gmail.com>
2024-01-24 19:56:18 -0500

    removed test cases for assertEqual

--- head.js
+++ head.js
@@ -1,4 +1,4 @@
-// FUNCTION IMPLEMENTATION
+// assertEqual function
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -6,12 +6,6 @@ const assertEqual = function(actual, expected) {
     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(15, 1);
-assertEqual("blue", "blue");
 
 //Returns the fist item in an array
 const head = function(arrayHead) {
