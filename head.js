abd3785f2640cb931e8e8a5cf9f3d6bc33260b1a
Leilani <pquaby@gmail.com>
2024-01-24 19:44:31 -0500

    added head function to assertEqual

---
+++ head.js
@@ -0,0 +1,27 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(15, 1);
+assertEqual("blue", "blue");
+
+//Returns the fist item in an array
+const head = function(arrayHead) {
+  return arrayHead[0];
+};
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5,6,7]), 7);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs");
+assertEqual(head([3]), 3);
+assertEqual(head([]), 5);
