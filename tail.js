dd46775da90e78a48885dad86cd0bd0d9911d574
Leilani <pquaby@gmail.com>
2024-01-24 20:36:02 -0500

    added tail function to assertEqual

---
+++ tail.js
@@ -0,0 +1,36 @@
+// assertEqual function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Returns the tail of an array
+const tail = function(arrayTail) {
+  let newArray = [];
+
+  for (let i = 1; i < arrayTail.length; i++) {
+    newArray.push(arrayTail[i]);
+  }
+
+  return newArray;
+};
+
+// TEST CASES
+const check = ["blue", "yellow", "green"];
+tail(check);
+assertEqual(check.length, 3); //original array shouldn't be modified
+
+const words = tail(["Yo Yo", "Lighthouse", "Labs"]);
+assertEqual(words.length, 2);
+assertEqual(words[0], "Lighthouse");
+assertEqual(words[1], "Labs");
+
+const numbers = tail([1, 2, 3, 4]);
+assertEqual(numbers.length, 3);
+assertEqual(numbers[0], 2);
+assertEqual(numbers[1], 3);
+assertEqual(numbers[2], 4);
+assertEqual(numbers[0], 5);
