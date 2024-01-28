b5131c872b91feaed3c4bc8d47761784142a101e
Leilani <pquaby@gmail.com>
2024-01-28 12:55:51 -0500

    added findKey function

---
+++ findKeyByValue.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return true;
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+    return false;
+  }
+};
+
+//Search for a given value in the object and return its key
+const findKeyByValue = function(obj, value) {
+  let foundKey = "";
+
+  for (const key in obj) {
+    if (obj[key] === value) {
+      foundKey = key;
+    } else {
+      foundKey = undefined;
+    }
+  }
+
+  return foundKey;
+};
+
+// Tests
+food = {fruit: "banana", vegetable: "onion", meat: "chicken"};
+test = {};
+
+assertEqual(findKeyByValue(food, "chicken"), "meat");
+assertEqual(findKeyByValue(food, "onion"), "vegetable");
+assertEqual(findKeyByValue(test, "banana"), "");
+assertEqual(findKeyByValue(food, "yum"), undefined);
