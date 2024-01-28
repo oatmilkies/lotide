4092da087748bcbb7b6062ac92dec20d053c1370
Leilani <pquaby@gmail.com>
2024-01-27 20:30:04 -0500

    created function that returns counts for a specific subset

---
+++ countOnly.js
@@ -0,0 +1,51 @@
+// Function will be given an array and an object. It will return an object containing counts of everything that the input object listed.
+//Compare 2 things and print if they match or not
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  //Create new object to store the count of each first name
+  const results = {};
+
+
+  //Only add the names in the argument object
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      //Set a property with that string key to the value that was already there (or zero if nothing there) with 1 added to it.
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+  return results;
+};
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
