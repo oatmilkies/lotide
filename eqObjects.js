f253d829eba288de41e16e09953cb9c98a72308d
Leilani <pquaby@gmail.com>
2024-01-30 18:18:05 -0500

    removed unnecessary return statements

--- eqObjects.js
+++ eqObjects.js
@@ -15,10 +15,8 @@ const eqArrays = function(arr1, arr2) {
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    return true;
   } else {
     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-    return false;
   }
 };
 
