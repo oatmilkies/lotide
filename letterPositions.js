188d80a4d3fe7d21f5c4e172dc047a2603901918
Leilani <pquaby@gmail.com>
2024-01-30 18:16:49 -0500

    removed unnecessary return statements

--- letterPositions.js
+++ letterPositions.js
@@ -1,10 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    return true;
   } else {
     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-    return false;
   }
 };
 
