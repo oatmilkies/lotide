76e43d714a9b4a74d6c914baacc90257ec9e57ff
Leilani <pquaby@gmail.com>
2024-02-01 12:49:39 -0500

    added map function

---
+++ map.js
@@ -0,0 +1,41 @@
+//Compare 2 arrays
+const eqArrays = function(arr1, arr2) {
+  //Check both arrays are the same length
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  //Compare each element
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+//Output to console if arrays are equal or not
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log("✅ Arrays are equal");
+  } else {
+    console.log("❌ Arrays are not equal");
+  }
+};
+
+const words = ["Ori", "Tunic", "Death's Door", "Jusant", "Citizen Sleeper"];
+//Return the first letter of each word in an array
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+// Tests
+assertArraysEqual(results1, [ 'O', 'T', 'D', 'J', 'C' ]);
+assertArraysEqual(results1[0], "O");
+assertArraysEqual(results1[2], "D");
