5cb91f762fbebb29e5c481967248f3fef98c8723
Leilani <pquaby@gmail.com>
2024-02-08 11:39:05 -0500

    cleaned up else statement

--- eqArrays.js
+++ eqArrays.js
@@ -20,11 +20,9 @@ const eqArrays = function(arr1, arr2) {
       if (!eqArrays(arr1[i], arr2[i])) {
         return false;
       }
-    } else {
       //If not an array, compare element values
-      if (arr1[i] !== arr2[i]) {
+    } else if (arr1[i] !== arr2[i]) {
         return false;
-      }
     }
   }
 
