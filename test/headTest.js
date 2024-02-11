5235ab277768b229ed3ea0534195189ecf12bc96
Leilani <pquaby@gmail.com>
2024-02-11 13:04:20 -0500

    added chai tests

--- test/headTest.js
+++ test/headTest.js
@@ -2,12 +2,24 @@
 
 const assertEqual = require('../assertEqual');
 const head = require('../head');
+const assert = require('chai').assert;
 
+describe("#head", () => {
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([3,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs");
-assertEqual(head([3]), 3);
-assertEqual(head([]), 5);
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns 'Hello' for [''Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+});
