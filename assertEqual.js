63b0de0580942dde02335dd93e97efc59d3eb580
Leilani <pquaby@gmail.com>
2024-02-11 12:05:21 -0500

    added module.exports

--- assertEqual.js
+++ assertEqual.js
@@ -7,8 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(15, 1);
-assertEqual("blue", "blue");
+module.exports = assertEqual;
