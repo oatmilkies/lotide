a6cd567ea8026f478ec016e7ab79e00b34ca902a
Leilani <pquaby@gmail.com>
2024-02-11 12:44:53 -0500

    fixed test

--- test/middleTest.js
+++ test/middleTest.js
@@ -5,14 +5,8 @@ const eqArrays = require('../eqArrays');
 const middle = require('../middle');
 
 //Test cases
-middle([1]); // => []
-middle([1, 2]); // => []
-
-middle([1, 2, 3]); // => [2]
-middle([1, 2, 3, 4, 5]); // => [3]
-
-middle([1, 2, 3, 4]); // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+assertArraysEqual(middle([1]), []); // => []
+assertArraysEqual(middle([1, 2]), []); // => []
 
 assertArraysEqual(middle([1, 2, 3]), [2]);
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
