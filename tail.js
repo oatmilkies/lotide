9f807f93976e53aabfc9da7954ae97fa4e94e83d
Leilani <pquaby@gmail.com>
2024-02-14 18:35:40 -0500

    fixed typo

--- tail.js
+++ tail.js
@@ -1,4 +1,4 @@
-// Returns the every element but the first element in an array
+// Returns every element but the first element in an array
 const tail = function(arrayTail) {
   return arrayTail.slice(1);
 };
